d175b4e63d6d84989474919f382c6112da8bd749
fractos <fractos@gmail.com>
2015-04-05 13:07:22 +0100

    adding inner exception chaining to constructor.

--- Inversion.Data/StoreStartedException.cs
+++ Inversion.Data/StoreStartedException.cs
@@ -6,7 +6,14 @@ namespace Inversion.Data
     [Serializable]
     public sealed class StoreStartedException : ApplicationException
     {
-        public StoreStartedException(string message) : base(message) { }
-        private StoreStartedException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+        public StoreStartedException(string message)
+            : base(message) { }
+
+        public StoreStartedException(string message, Exception innerException)
+            : base(message, innerException) { }
+
+        private StoreStartedException(
+            SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
     }
 }
