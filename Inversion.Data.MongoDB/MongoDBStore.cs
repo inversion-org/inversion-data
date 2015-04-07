3d752069f9de4d2d8f28477e6fbd520f2c0084ab
fractos <fractos@gmail.com>
2015-04-07 21:51:43 +0100

    adding mongodb store.

---
+++ Inversion.Data.MongoDB/MongoDBStore.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Inversion.Data
+{
+    public class MongoDBStore : Store
+    {
+        private readonly MongoClient _client;
+        private readonly IMongoDatabase _db;
+
+        public IMongoCollection<BsonDocument> this[string collectionName]
+        {
+            get { return this.Database.GetCollection<BsonDocument>(collectionName); }
+        }
+
+        public MongoClient Client
+        {
+            get { return _client; }
+        }
+
+        public IMongoDatabase Database
+        {
+            get { return _db; }
+        }
+
+        public MongoDBStore(string connStr, string dbName)
+        {
+            _client = new MongoClient(connStr);
+            _db = _client.GetDatabase(dbName);
+        }
+
+        public sealed override void Dispose()
+        {
+            // nothing to do
+        }
+    }
+}
