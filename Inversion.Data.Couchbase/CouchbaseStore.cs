9e01df82e89c92401fd8b5a574be033c07a9b816
Adam Christie <fractos@gmail.com>
2015-04-10 00:03:32 +0100

    Adding Couchbase and Redis support.

---
+++ Inversion.Data.Couchbase/CouchbaseStore.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Couchbase;
+using Couchbase.Configuration.Client;
+using Couchbase.Core;
+
+namespace Inversion.Data.Couchbase
+{
+    public class CouchbaseStore<T> : Store
+    {
+        protected readonly Cluster Cluster;
+        protected IBucket Bucket;
+        protected readonly string BucketName;
+        private bool _disposed;
+
+        public Document<T> this[string key]
+        {
+            get
+            {
+                IDocumentResult<T> result = this.Bucket.GetDocument<T>(key);
+                return result.Success ? result.Document : null;
+            }
+            set
+            {
+                this.Bucket.Upsert(value);
+            }
+        }
+
+        public CouchbaseStore(IEnumerable<string> uris, string bucketName)
+        {
+            this.Cluster = new Cluster(
+                new ClientConfiguration
+                {
+                    Servers = new List<Uri>(uris.Select(u => new Uri(u)))
+                }
+            );
+            this.BucketName = bucketName;
+        }
+
+        public override void Start()
+        {
+            base.Start();
+            this.Bucket = this.Cluster.OpenBucket(this.BucketName);
+        }
+
+        public sealed override void Dispose()
+        {
+            if (_disposed) return;
+
+            _disposed = true;
+
+            if (this.HasStarted)
+            {
+                if (this.Bucket != null)
+                {
+                    this.Bucket.Dispose();
+                }
+            }
+
+            if (this.Cluster != null)
+            {
+                this.Cluster.Dispose();
+            }
+        }
+    }
+}
---
+++ Inversion.Data.Redis/RedisStore.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace Inversion.Data.Redis
+{
+    public class RedisStore : Store
+    {
+        private readonly string _connections;
+
+        protected ConnectionMultiplexer ConnectionMultiplexer { get; private set; }
+
+        private readonly bool _oneUse;
+        private bool _disposed;
+
+        public RedisStore(string connections)
+        {
+            _connections = connections;
+            _oneUse = true;
+        }
+
+        public RedisStore(ConnectionMultiplexer connectionMultiplexer)
+        {
+            this.ConnectionMultiplexer = connectionMultiplexer;
+            _oneUse = false;
+        }
+
+        public override void Start()
+        {
+            base.Start();
+
+            if (this.ConnectionMultiplexer != null)
+            {
+                this.ConnectionMultiplexer = ConnectionMultiplexer.Connect(_connections);
+            }
+        }
+
+        public override void Dispose()
+        {
+            if (_disposed) return;
+
+            _disposed = true;
+
+            if (_oneUse)
+            {
+                this.ConnectionMultiplexer.Dispose();
+            }
+        }
+    }
+}
