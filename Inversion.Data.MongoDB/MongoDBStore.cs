56506ae04ac3e1b915e475936d369e8e0e02a1e0
Adam Christie <fractos@gmail.com>
2015-04-17 00:06:36 +0100

    better version of mongodbstore.

--- Inversion.Data.MongoDB/MongoDBStore.cs
+++ Inversion.Data.MongoDB/MongoDBStore.cs
@@ -5,18 +5,36 @@ namespace Inversion.Data
 {
     public class MongoDBStore : Store
     {
-        private readonly MongoClient _client;
-        protected readonly IMongoDatabase Database;
+        private MongoClient _client;
+        protected IMongoDatabase Database;
+        private readonly string _connStr;
+        private readonly string _dbName;
 
         public IMongoCollection<BsonDocument> this[string collectionName]
         {
-            get { return this.Database.GetCollection<BsonDocument>(collectionName); }
+            get
+            {
+                if (!this.HasStarted)
+                {
+                    throw new StoreProcessException("The store must be started to use it.");
+                }
+
+                return this.Database.GetCollection<BsonDocument>(collectionName);
+            }
         }
 
         public MongoDBStore(string connStr, string dbName)
         {
-            _client = new MongoClient(connStr);
-            this.Database = _client.GetDatabase(dbName);
+            _connStr = connStr;
+            _dbName = dbName;
+        }
+
+        public override void Start()
+        {
+            base.Start();
+
+            _client = new MongoClient(_connStr);
+            this.Database = _client.GetDatabase(_dbName);
         }
 
         public sealed override void Dispose()
