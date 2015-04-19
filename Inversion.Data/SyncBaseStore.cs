583b02f93352c8452893b664c7d0b2f774740b1d
Adam Christie <fractos@gmail.com>
2015-04-19 21:04:43 +0100

    adding syncbase store.

---
+++ Inversion.Data/SyncBaseStore.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+
+namespace Inversion.Data
+{
+    public abstract class SyncBaseStore : Store
+    {
+        private readonly ReaderWriterLockSlim _lockSlim = new ReaderWriterLockSlim();
+
+        protected ReaderWriterLockSlim LockSlim { get { return _lockSlim; } }
+    }
+}
