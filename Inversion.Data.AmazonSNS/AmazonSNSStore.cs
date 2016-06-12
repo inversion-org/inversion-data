56dd683dd4b386903068ce1e556125b618e6c80f
Adam Christie <fractos@gmail.com>
2016-06-12 11:58:55 +0100

    adding amazon sns store.

---
+++ Inversion.Data.AmazonSNS/AmazonSNSStore.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Amazon.Runtime;
+using Amazon.SimpleNotificationService;
+
+namespace Inversion.Data
+{
+    public class AmazonSNSStore : StoreBase, IStoreHealth
+    {
+        protected readonly string TopicArn;
+        protected readonly string Region;
+        protected AmazonSimpleNotificationServiceClient Client;
+        private readonly string _accessKey;
+        private readonly string _accessSecret;
+
+        private bool _disposed;
+
+        public AmazonSNSStore(string topicArn, string region, string accessKey, string accessSecret)
+        {
+            this.TopicArn = topicArn;
+            this.Region = region;
+            _accessKey = accessKey;
+            _accessSecret = accessSecret;
+        }
+
+        public override void Start()
+        {
+            base.Start();
+
+            AWSCredentials credentials = new BasicAWSCredentials(_accessKey, _accessSecret);
+            AmazonSimpleNotificationServiceConfig config = new AmazonSimpleNotificationServiceConfig
+            {
+                RegionEndpoint = Amazon.RegionEndpoint.GetBySystemName(this.Region)
+            };
+            this.Client = new AmazonSimpleNotificationServiceClient(credentials, config);
+        }
+
+        public sealed override void Dispose()
+        {
+            if (_disposed) return;
+
+            _disposed = true;
+
+            if (this.HasStarted)
+            {
+                if (this.Client != null)
+                {
+                    this.Client.Dispose();
+                }
+            }
+        }
+
+        public bool GetHealth(out string result)
+        {
+            this.AssertIsStarted();
+
+            result = "not supported";
+
+            return true;
+        }
+    }
+}
