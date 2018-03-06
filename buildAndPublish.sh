cb7294dc457680ebb67affe826440d3bc300fd84
Adam Christie <fractos@gmail.com>
2018-03-06 23:27:53 +0000

    typos in build script and removing harness from sln.

--- buildAndPublish.sh
+++ buildAndPublish.sh
@@ -1,39 +1,37 @@
 #!/bin/bash
 
-dotnet restore
-
 dotnet build
 
 dotnet pack
 
-pushd Inversion.Data/bin/debug
+pushd Inversion.Data/bin/Debug
   dotnet nuget push *.nupkg -k $NUGET_KEY -s https://api.nuget.org/v3/index.json
 popd
 
-pushd Inversion.Data.AmazonSNS/bin/debug
+pushd Inversion.Data.AmazonSNS/bin/Debug
   dotnet nuget push *.nupkg -k $NUGET_KEY -s https://api.nuget.org/v3/index.json
 popd
 
-pushd Inversion.Data.AmazonSQS/bin/debug
+pushd Inversion.Data.AmazonSQS/bin/Debug
   dotnet nuget push *.nupkg -k $NUGET_KEY -s https://api.nuget.org/v3/index.json
 popd
 
-pushd Inversion.Data.Couchbase/bin/debug
+pushd Inversion.Data.Couchbase/bin/Debug
   dotnet nuget push *.nupkg -k $NUGET_KEY -s https://api.nuget.org/v3/index.json
 popd
 
-pushd Inversion.Data.DynamoDB/bin/debug
+pushd Inversion.Data.DynamoDB/bin/Debug
   dotnet nuget push *.nupkg -k $NUGET_KEY -s https://api.nuget.org/v3/index.json
 popd
 
-pushd Inversion.Data.MongoDB/bin/debug
+pushd Inversion.Data.MongoDB/bin/Debug
   dotnet nuget push *.nupkg -k $NUGET_KEY -s https://api.nuget.org/v3/index.json
 popd
 
-pushd Inversion.Data.Redis/bin/debug
+pushd Inversion.Data.Redis/bin/Debug
   dotnet nuget push *.nupkg -k $NUGET_KEY -s https://api.nuget.org/v3/index.json
 popd
 
-pushd Inversion.Data.Sql/bin/debug
+pushd Inversion.Data.Sql/bin/Debug
   dotnet nuget push *.nupkg -k $NUGET_KEY -s https://api.nuget.org/v3/index.json
 popd
